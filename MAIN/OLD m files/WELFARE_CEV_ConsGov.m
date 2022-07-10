372ad643dcf924a1e6c1fd3a9833313b29920bb0
MaximeBrn <37322244+MaximeBrn@users.noreply.github.com>
2022-07-10 16:49:06 +0200

    Clean repository
    We remove old files and folder

--- MAIN/OLD m files/WELFARE_CEV_ConsGov.m
+++
@@ -1,7 +0,0 @@
-function res = WELFARE_CEV_ConsGov(CEV,h,CHI_C,CHI_G,SIGMA,PHI,GAMMA,C_vec,N_vec,G_vec,C_starr_vec,N_starr_vec,G_starr_vec)
-    
-    res = WELFARE(h,CHI_C,CHI_G,SIGMA,PHI,GAMMA,CEV*C_vec,N_vec,CEV*G_vec,CEV*C_starr_vec,N_starr_vec,CEV*G_starr_vec);
-
-end
-
- 
